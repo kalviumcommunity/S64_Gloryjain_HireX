5bd3afbbc316701a1e91b493a3375dd67149fde0
GLORY-JAIN <glory.jain@kalvium.community>
2025-04-12 14:55:11 +0530

    added get

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,21 @@
-const express = require("express");
-
+import express from "express";
+import userRoutes from "./routes/user.routes.js";
 
 const app = express();
-
 const PORT = 5000;
 
+// Middleware to parse JSON
+app.use(express.json());
+
+// Routes
+app.use('/api/users', userRoutes);
+
+// Test route
 app.get('/', (req, res) => {
-    res.send('Api is running...');
+    res.send('API is running...');
 });
 
-
+// Start server
 app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
-})
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
